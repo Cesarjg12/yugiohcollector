3ca0e5a63573a8fe3c9c04ec76bac38b1c38eb98
cesarjg28 <cesarjg12@yahoo.com>
2023-07-25 16:01:47 -0700

    proceeding with steps

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+yugioh = [
+  {'name': 'Card', 'type': 'monster', 'description': 'furry little demon', 'stars': 3},
+  {'name': 'Card2', 'type': 'monster', 'description': 'has a hat', 'stars': 8},
+ 
+]
+
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def yugioh_index(request):
+  return render(request, 'yugioh/index.html', {
+    'yugioh': yugioh
+  })
