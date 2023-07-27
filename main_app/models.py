2145a0df1d490eff2f78e9cf81f5ca20e78cf708
cesarjg28 <cesarjg12@yahoo.com>
2023-07-27 13:50:17 -0700

    made a buff feature replacing cat feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+from django.urls import reverse
+
+EFFECTS = (
+  ('M', 'Monster'),
+  ('S', 'Spell'),
+  ('T', 'Trap'),
+)
+
 # Create your models here.
 class Yugioh(models.Model):
     name = models.CharField(max_length=100)
@@ -12,3 +20,22 @@ class Yugioh(models.Model):
 # no makemigrations is necessary
 def __str__(self):
     return f'{self.name} ({self.id})'
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'cat_id': self.id})
+
+class Buffs(models.Model):
+  date = models.DateField()
+  effect = models.CharField(
+    max_length=1,
+    choices=EFFECTS,
+    default=EFFECTS[0][0]
+  )
+  # Create a yugioh_id FK
+  yugioh = models.ForeignKey(
+    Yugioh,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"{self.get_effect_display()} on {self.date}"
